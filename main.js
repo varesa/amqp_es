45492aa3d1a516dfd066682ccd2fca62748b6202
Esa Varemo <esa@kuivanto.fi>
2016-05-24 10:19:13 +0300

    amqp to elasticsearch

--- main.js
+++ main.js
@@ -1,46 +1,49 @@
-/**
- * Created by E on 23.5.2016.
- */
 
 
-var channel = process.env.channel;
+
 var rabbitmq_host = process.env.rabbitmq_host;
+var elastic_host = process.env.elastic_host;
 
-var irc_server = process.env.irc_server;
-var irc_user = process.env.irc_user;
 
-/* Twitch */
+/* Elasticsearch */
 
-var irc = require('irc');
-var client = new irc.Client(irc_server, irc_user, {
-    channels: ['#' + channel]
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: elastic_host,
+  log: 'trace'
 });
 
-/* RabbitMQ */
 
-var rabbitmq_ch;
-var exchange = "amqp.irc." + channel;
+/* RabbitMQ */
 
 var amqp = require("amqplib/callback_api");
 amqp.connect("amqp://" + rabbitmq_host, function (err, conn) {
     conn.createChannel(function (err, ch) {
-        ch.assertExchange(exchange, "fanout", {durable: false});
-        rabbitmq_ch = ch;
+        var exchange = "amqp.irc";
+        ch.assertExchange(exchange, "topic", {durable: false});
+
+        ch.assertQueue('', {exclusive: true}, function (err, q) {
+            ch.bindQueue(q.queue, exchange, "messages.#");
+            ch.consume(q.queue, function (msg) {
+                var index = "irc";
+                if(msg.fields.routingKey.startsWith("messages.twitch")) {
+                    index = "twitch";
+                }
+
+                var obj = JSON.parse(msg.content.toString("utf-8"));
+                obj.date = new Date(obj.time).toISOString();
+
+                client.create({
+                    index: index,
+                    type: "chat",
+                    body: obj
+                }, function (err, resp) {
+                    console.log(err);
+                    console.log(resp);
+                });
+            });
+        });
+
+
     })
-});
-
-/* Connector */
-
-client.addListener('message', function (from, to, message) {
-    var time = Date.now();
-    var object = {
-        type: "messsage",
-        channel: to,
-        user: from,
-        message: message,
-        time: time
-    };
-    if(rabbitmq_ch) {
-        rabbitmq_ch.publish(exchange, "", new Buffer(JSON.stringify(object)));
-    }
 });
